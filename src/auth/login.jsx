d3ea9b29cf36b55d607dc4091ef76687aa368f28
Jesus Jo <32987141+jesusj0o@users.noreply.github.com>
2022-01-10 14:22:01 -0700

    Login design

---
+++ src/auth/login.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './login.css'
+const Login = () => {
+    return ( 
+        <div className='wrapper'>
+            <div className='form-content'>
+                <form action="submit">
+                    <div className='custom-brand'>
+                        <h3 className='brand'>Login</h3>
+                    </div>                   
+                    <input type="text" className='custom-login-input' placeholder='Username' />
+                    <input type="password" className='custom-login-input' placeholder='Password'/> 
+                    <input type="submit" className='btn-login' value={'Log In'} />   
+                </form>
+                <div className='form-footer'>
+                    <a className='underlineHover'>Forgot password?</a>
+                </div>
+            </div>
+            
+        </div>
+    );
+}
+export default Login;
